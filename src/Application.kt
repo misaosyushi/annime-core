e8d866503dcfc6a547c5bb80812f332e76e34056
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-04-30 09:35:03 +0900

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,22 @@
+package com.annime.core
+
+import io.ktor.application.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respond(HttpStatusCode.OK,"Hello, Kotlin")
+            }
+        }
+    }
+    server.start()
+}
+
